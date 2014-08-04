5e383744d32115a8473663ae7199a45b893b373d
YuzuruS <navitima@gmail.com>
2014-08-04 13:31:09 +0900

    add twitter_account

---
+++ twitter_account1/search.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'oauth.php';
+require_once 'define.php';
+include '../common/search.php';
---
+++ twitter_account2/define.php
@@ -0,0 +1,15 @@
+<?php
+// FOR ALL
+define('SLEEP_TIME_SEC', 60);
+define('FOLLOW_UPPER_LIMIT', 20);
+
+// FOR SEARCH
+define('SEARCH_QUERRY', 'マンガ読みたい');
+define('SEARCH_CNT', 100);
+
+// FOR UNFOLLOW
+define('LANG', 'ja');
+define('UNFOLLOW_UPPER_LIMIT', 20);
+
+// FOR POST
+define('TWEET_FILE_NAME', 'tweet.txt');
