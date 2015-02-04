3b74aeb0ee4d8d15ae9f38e041f1336923424bbd
YuzuruS <navitima@gmail.com>
2015-02-04 10:46:14 +0900

    fix copy.sh

--- copy.sh
+++ copy.sh
@@ -3,9 +3,5 @@
 main="twitter_account1"
 for dir in twitter_account2
 do
-cp $main/search.php $dir
-cp $main/post.php $dir
-cp $main/back.php $dir
-cp $main/unfollow.php $dir
-cp $main/define.php $dir
+cp $main/*.php $dir
 done
